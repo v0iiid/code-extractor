efbabfa229ae2bf8d9ac0464a2d9a4faa1c8c84a
mikeyyy12 <shubhamkarmyal95@gmail.com>
2026-01-19 21:01:15 +0530

    update:create folder if not exists

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,7 +1,16 @@
 import subprocess
 import time
+import os
 
 start = time.time()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+frames_dir = os.path.join(BASE_DIR, "frames")
+
+if not os.path.exists(frames_dir):
+    os.makedirs(frames_dir)
+print(f"Frames folder path: {frames_dir}")
 
 subprocess.run([
     "ffmpeg",
